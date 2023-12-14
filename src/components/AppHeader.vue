90c11578cad41759cc9f0b5180fd373533575109
JohnQuimson <john.quimson777@gmail.com>
2023-12-14 14:37:13 +0100

    feat: milestone 0 & README

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>funziona tutto</template>
+
+<style scoped lang="scss">
+@use '../assets/scss/style.scss' as *;
+</style>
