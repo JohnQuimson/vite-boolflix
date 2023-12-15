10c64cb4d59d805216f7b5bcf2345d0826b105e7
JohnQuimson <john.quimson777@gmail.com>
2023-12-15 17:45:40 +0100

    feat: add flags

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -21,10 +21,26 @@ export default {
     <div>
       <h3>{{ titolo }}</h3>
       <h4>{{ titOrig }}</h4>
-      <p>{{ lingua }}</p>
+
+      <p v-if="lingua === 'it'">
+        {{ lingua }}
+        <img src="../../public/img/it-flag.jpg" alt="it-flag" />
+      </p>
+      <p v-else-if="lingua === 'en'">
+        {{ lingua }}
+        <img src="../../public/img/en-flag.jpg" alt="en-flag" />
+      </p>
+      <p v-else="lingua === 'en'">
+        {{ lingua }}
+      </p>
+
       <p>{{ voto }}</p>
     </div>
   </li>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+img {
+  width: 50px;
+}
+</style>
